861196fc1a980a3a1bc9a34083ae2752893015ef
Erickkirui <erickkirui653@gmail.com>
2023-03-22 14:11:34 +0300

    Remove the return values on if statements

--- netsalary.js
+++ netsalary.js
@@ -2,93 +2,92 @@
 //Calculate the payee (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary. 
 
 function calculateNetSalary(basicSalary){
+
+    // culculate gross salary 
+    let grossSalary = basicSalary + benefits
+
     // culculate benefits
     let benefits = ""
     
     // culculate NSSF Using the old rates
     let nssfDeduction = (5/100 * grossSalary*2)
-    
-
-    // culculate gross salary 
-    let grossSalary = basicSalary + benefits
-    
 
     // culculate PAYE
     if (basicSalary < 24000){
 
-        return payAsYouEarn = (basicSalary * 10/100)
+     payAsYouEarn = (basicSalary * 10/100)
     }
     else if (basicSalary > 24000 && basicSalary < 32333){
 
-        return payAsYouEarn = (basicSalary* 25/100)
+      payAsYouEarn = (basicSalary* 25/100)
     }
     else {
-     return payAsYouEarn = (basicSalary * 30/100)
+      payAsYouEarn = (basicSalary * 30/100)
     }
 
     //culculate NHIF deductions 
     let nhifDeductions = 0
     if (basicSalary < 5999){
         
-        return nhifDeductions = 150
+     nhifDeductions = 150
 
     }
     else if ( basicSalary > 6000 && basicSalary < 7999){
 
-        return nhifDeductions = 300
+      nhifDeductions = 300
 
     }
     else if(basicSalary > 8000 && basicSalary < 11999){
 
-       return  nhifDeductions = 400   
+       nhifDeductions = 400   
 
     }
     else if( basicSalary > 12000 && basicSalary < 14999 ){
 
-        return nhifDeductions = 500
+         nhifDeductions = 500
     }
 
     else if (basicSalary > 15000 && basicSalary < 19999) {
 
-           return nhifDeductions = 600
+           nhifDeductions = 600
     }
     else if (basicSalary > 20000 && basicSalary < 24999){
-        return nhifDeductions = 750
+      nhifDeductions = 750
     }
     else if (basicSalary > 25000 && basicSalary < 29999){
 
-        return nhifDeductions = 850
+        nhifDeductions = 850
     }
     else if (basicSalary > 30000 && basicSalary < 34999){
 
-        return nhifDeductions = 900
+         nhifDeductions = 900
     }
     else if (basicSalary > 35000 && basicSalary < 39999){
-        return nhifDeductions = 950
+         nhifDeductions = 950
     }
     else if (basicSalary > 40000 && basicSalary < 44999){
-        return nhifDeductions = 1000
+       nhifDeductions = 1000
     }
     else if (basicSalary > 45000  && basicSalary < 49999){
-        return nhifDeductions =  1100
+       nhifDeductions =  1100
     }
     else if (basicSalary > 50000 && basicSalary < 59999){
-        return nhifDeductions = 1200
+       nhifDeductions = 1200
     }
     else if (basicSalary > 60000 && basicSalary < 69999){
-        return nhifDeductions = 1300
+        nhifDeductions = 1300
     }
     else if (basicSalary > 70000 && basicSalary < 79999){
-        return nhifDeductions = 1400
+         nhifDeductions = 1400
     }
     else if (basicSalary > 80000 && basicSalary < 89999){
-        return nhifDeductions = 1300
+         nhifDeductions = 1300
     }
     else if (basicSalary > 90000 && basicSalary < 99999){
-        return nhifDeductions = 1300
+         nhifDeductions = 1300
     }
     else {
-        return nhifDeductions = 1700
+         nhifDeductions = 1700
     }
 
     //
